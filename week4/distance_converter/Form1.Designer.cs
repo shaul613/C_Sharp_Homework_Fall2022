b0dec2a55635bf65fcad224718275c8174125f02
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-11-02 07:05:30 -0400

    exit button

--- week4/distance_converter/Form1.Designer.cs
+++ week4/distance_converter/Form1.Designer.cs
@@ -117,6 +117,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
