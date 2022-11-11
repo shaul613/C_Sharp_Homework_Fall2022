85639894e1a450eab11934a0eca70eeae307f83e
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-11-11 09:16:44 -0500

    cmt

--- week5/tuition_increase/Form1.Designer.cs
+++ week5/tuition_increase/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tuitionList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tuitionList
+            // 
+            this.tuitionList.FormattingEnabled = true;
+            this.tuitionList.ItemHeight = 20;
+            this.tuitionList.Location = new System.Drawing.Point(127, 122);
+            this.tuitionList.Name = "tuitionList";
+            this.tuitionList.Size = new System.Drawing.Size(120, 84);
+            this.tuitionList.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tuitionList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox tuitionList;
     }
 }
 
--- week5/tuition_increase/Form1.cs
+++ week5/tuition_increase/Form1.cs
@@ -16,5 +16,10 @@ namespace tuition_increase
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tuitionList.Items.Add("hello");
+        }
     }
 }
