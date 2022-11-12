665702953c0252c048150b7c59c693403060320b
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-11-12 16:05:11 -0500

    added comments

--- week5/tuition_increase/Form1.cs
+++ week5/tuition_increase/Form1.cs
@@ -19,10 +19,12 @@ namespace tuition_increase
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            double tuition = 2000;
-            for(int i = 0; i < 6; i++)
+            double tuition = 2000; // Tuition amount for year 1
+            for(int i = 0; i < 6; i++) // Current year and next 5 years
             {
+                // Displaying tuition amount in listbox
                 tuitionList.Items.Add("Tuition for  year " + (i+1) + " - " + tuition);
+                // Increasing tuition variable by 1.02 (2%)
                 tuition *= 1.02;
             }
             
