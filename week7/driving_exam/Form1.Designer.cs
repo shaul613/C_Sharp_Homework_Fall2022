3f5525ced81969a54fbc8c6a23175e529e294cd6
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-11-28 16:05:38 -0500

    DL exam project completed

--- week7/driving_exam/Form1.Designer.cs
+++ week7/driving_exam/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.result = new System.Windows.Forms.TextBox();
+            this.ans_pass = new System.Windows.Forms.TextBox();
+            this.ans_fail = new System.Windows.Forms.TextBox();
+            this.all_fail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(44, 41);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(195, 26);
+            this.result.TabIndex = 0;
+            this.result.Text = "Result: ";
+            // 
+            // ans_pass
+            // 
+            this.ans_pass.Location = new System.Drawing.Point(256, 41);
+            this.ans_pass.Name = "ans_pass";
+            this.ans_pass.ReadOnly = true;
+            this.ans_pass.Size = new System.Drawing.Size(195, 26);
+            this.ans_pass.TabIndex = 1;
+            this.ans_pass.Text = "Total Correct: ";
+            // 
+            // ans_fail
+            // 
+            this.ans_fail.Location = new System.Drawing.Point(469, 41);
+            this.ans_fail.Name = "ans_fail";
+            this.ans_fail.ReadOnly = true;
+            this.ans_fail.Size = new System.Drawing.Size(195, 26);
+            this.ans_fail.TabIndex = 2;
+            this.ans_fail.Text = "Total Incorrect: ";
+            // 
+            // all_fail
+            // 
+            this.all_fail.Location = new System.Drawing.Point(44, 83);
+            this.all_fail.Multiline = true;
+            this.all_fail.Name = "all_fail";
+            this.all_fail.ReadOnly = true;
+            this.all_fail.Size = new System.Drawing.Size(620, 29);
+            this.all_fail.TabIndex = 3;
+            this.all_fail.Text = "List of wrong answers: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.all_fail);
+            this.Controls.Add(this.ans_fail);
+            this.Controls.Add(this.ans_pass);
+            this.Controls.Add(this.result);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox ans_pass;
+        private System.Windows.Forms.TextBox ans_fail;
+        private System.Windows.Forms.TextBox all_fail;
     }
 }
 
--- week7/driving_exam/Form1.cs
+++ week7/driving_exam/Form1.cs
@@ -16,13 +16,43 @@ namespace driving_exam
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show(text);
+            Dictionary<string, string> data = getResults(correctAnswers, submittedAnswers);
+            result.Text += data["result"];
+            ans_pass.Text += data["correct"];
+            ans_fail.Text += data["incorrect"];
+            all_fail.Text += data["incorrectList"];
+
         }
 
-        public String[] answers = "b d a a c a b a c d b c d a d c c b d a".Split();
-        string text = System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "..\\..\\answers.txt"));
+        public string[] correctAnswers = "b d a a c a b a c d b c d a d c c b d a".Split();
+        public string[] submittedAnswers = System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "..\\..\\answers.txt")).Split(',');
         
-        
+        public static Dictionary<string, string> getResults(string[] answers, string[] submission)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            int correct = 0;
+            int incorrect = 0;
+            string incorrectList = "";
+            for(int i = 0; i < 20; i++)
+            {
+                if (answers[i] == submission[i])
+                {
+                    correct++;
+                }
+                else
+                {
+                    incorrect++;
+                    incorrectList += i.ToString() + " ";
+                }
+            }
+            string passFail = correct >= 15 ? "Pass" : "Fail";
+            result.Add("result", passFail);
+            result.Add("correct", correct.ToString());
+            result.Add("incorrect", incorrect.ToString());
+            result.Add("incorrectList", incorrectList);
+
+            return result;
+        }
         
     }
 }
