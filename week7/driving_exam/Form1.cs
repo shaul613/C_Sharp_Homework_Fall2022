c75af9806f28b6d4fc13b6926eddaf5deec2db17
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-11-28 15:18:28 -0500

    cmt

--- week7/driving_exam/Form1.cs
+++ week7/driving_exam/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,13 @@ namespace driving_exam
         public Form1()
         {
             InitializeComponent();
+            MessageBox.Show(text);
         }
 
         public String[] answers = "b d a a c a b a c d b c d a d c c b d a".Split();
+        string text = System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "..\\..\\answers.txt"));
+        
+        
         
     }
 }
