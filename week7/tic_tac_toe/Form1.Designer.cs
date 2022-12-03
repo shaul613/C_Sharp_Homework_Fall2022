a0ea5117fad5af19d2347f96f44c18eecdc3adcf
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-12-03 15:22:16 -0500

    completed

--- week7/tic_tac_toe/Form1.Designer.cs
+++ week7/tic_tac_toe/Form1.Designer.cs
@@ -31,15 +31,15 @@
             this.new_game_btn = new System.Windows.Forms.Button();
             this.exit_btn = new System.Windows.Forms.Button();
             this.winner_box = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.b0 = new System.Windows.Forms.TextBox();
+            this.b8 = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.TextBox();
+            this.b6 = new System.Windows.Forms.TextBox();
+            this.b5 = new System.Windows.Forms.TextBox();
+            this.b4 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // new_game_btn
@@ -50,6 +50,7 @@
             this.new_game_btn.TabIndex = 0;
             this.new_game_btn.Text = "New Game";
             this.new_game_btn.UseVisualStyleBackColor = true;
+            this.new_game_btn.Click += new System.EventHandler(this.new_game_btn_Click);
             // 
             // exit_btn
             // 
@@ -59,6 +60,7 @@
             this.exit_btn.TabIndex = 1;
             this.exit_btn.Text = "Exit";
             this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
             // 
             // winner_box
             // 
@@ -67,101 +69,101 @@
             this.winner_box.Size = new System.Drawing.Size(220, 26);
             this.winner_box.TabIndex = 2;
             // 
-            // textBox1
+            // b0
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 47);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(115, 115);
-            this.textBox1.TabIndex = 3;
+            this.b0.Location = new System.Drawing.Point(104, 47);
+            this.b0.Multiline = true;
+            this.b0.Name = "b0";
+            this.b0.ReadOnly = true;
+            this.b0.Size = new System.Drawing.Size(115, 115);
+            this.b0.TabIndex = 3;
             // 
-            // textBox2
+            // b8
             // 
-            this.textBox2.Location = new System.Drawing.Point(422, 345);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(115, 115);
-            this.textBox2.TabIndex = 4;
+            this.b8.Location = new System.Drawing.Point(422, 345);
+            this.b8.Multiline = true;
+            this.b8.Name = "b8";
+            this.b8.ReadOnly = true;
+            this.b8.Size = new System.Drawing.Size(115, 115);
+            this.b8.TabIndex = 4;
             // 
-            // textBox3
+            // b7
             // 
-            this.textBox3.Location = new System.Drawing.Point(265, 345);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(115, 115);
-            this.textBox3.TabIndex = 5;
+            this.b7.Location = new System.Drawing.Point(265, 345);
+            this.b7.Multiline = true;
+            this.b7.Name = "b7";
+            this.b7.ReadOnly = true;
+            this.b7.Size = new System.Drawing.Size(115, 115);
+            this.b7.TabIndex = 5;
             // 
-            // textBox4
+            // b6
             // 
-            this.textBox4.Location = new System.Drawing.Point(104, 345);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(115, 115);
-            this.textBox4.TabIndex = 6;
+            this.b6.Location = new System.Drawing.Point(104, 345);
+            this.b6.Multiline = true;
+            this.b6.Name = "b6";
+            this.b6.ReadOnly = true;
+            this.b6.Size = new System.Drawing.Size(115, 115);
+            this.b6.TabIndex = 6;
             // 
-            // textBox5
+            // b5
             // 
-            this.textBox5.Location = new System.Drawing.Point(422, 197);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(115, 115);
-            this.textBox5.TabIndex = 7;
+            this.b5.Location = new System.Drawing.Point(422, 197);
+            this.b5.Multiline = true;
+            this.b5.Name = "b5";
+            this.b5.ReadOnly = true;
+            this.b5.Size = new System.Drawing.Size(115, 115);
+            this.b5.TabIndex = 7;
             // 
-            // textBox6
+            // b4
             // 
-            this.textBox6.Location = new System.Drawing.Point(265, 197);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(115, 115);
-            this.textBox6.TabIndex = 8;
+            this.b4.Location = new System.Drawing.Point(265, 197);
+            this.b4.Multiline = true;
+            this.b4.Name = "b4";
+            this.b4.ReadOnly = true;
+            this.b4.Size = new System.Drawing.Size(115, 115);
+            this.b4.TabIndex = 8;
             // 
-            // textBox7
+            // b3
             // 
-            this.textBox7.Location = new System.Drawing.Point(104, 197);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.ReadOnly = true;
-            this.textBox7.Size = new System.Drawing.Size(115, 115);
-            this.textBox7.TabIndex = 9;
+            this.b3.Location = new System.Drawing.Point(104, 197);
+            this.b3.Multiline = true;
+            this.b3.Name = "b3";
+            this.b3.ReadOnly = true;
+            this.b3.Size = new System.Drawing.Size(115, 115);
+            this.b3.TabIndex = 9;
             // 
-            // textBox8
+            // b2
             // 
-            this.textBox8.Location = new System.Drawing.Point(422, 47);
-            this.textBox8.Multiline = true;
-            this.textBox8.Name = "textBox8";
-            this.textBox8.ReadOnly = true;
-            this.textBox8.Size = new System.Drawing.Size(115, 115);
-            this.textBox8.TabIndex = 10;
+            this.b2.Location = new System.Drawing.Point(422, 47);
+            this.b2.Multiline = true;
+            this.b2.Name = "b2";
+            this.b2.ReadOnly = true;
+            this.b2.Size = new System.Drawing.Size(115, 115);
+            this.b2.TabIndex = 10;
             // 
-            // textBox9
+            // b1
             // 
-            this.textBox9.Location = new System.Drawing.Point(265, 47);
-            this.textBox9.Multiline = true;
-            this.textBox9.Name = "textBox9";
-            this.textBox9.ReadOnly = true;
-            this.textBox9.Size = new System.Drawing.Size(115, 115);
-            this.textBox9.TabIndex = 11;
+            this.b1.Location = new System.Drawing.Point(265, 47);
+            this.b1.Multiline = true;
+            this.b1.Name = "b1";
+            this.b1.ReadOnly = true;
+            this.b1.Size = new System.Drawing.Size(115, 115);
+            this.b1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(697, 675);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b0);
             this.Controls.Add(this.winner_box);
             this.Controls.Add(this.exit_btn);
             this.Controls.Add(this.new_game_btn);
@@ -178,15 +180,15 @@
         private System.Windows.Forms.Button new_game_btn;
         private System.Windows.Forms.Button exit_btn;
         private System.Windows.Forms.TextBox winner_box;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox b0;
+        private System.Windows.Forms.TextBox b8;
+        private System.Windows.Forms.TextBox b7;
+        private System.Windows.Forms.TextBox b6;
+        private System.Windows.Forms.TextBox b5;
+        private System.Windows.Forms.TextBox b4;
+        private System.Windows.Forms.TextBox b3;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox b1;
     }
 }
 
--- week7/tic_tac_toe/Form1.cs
+++ week7/tic_tac_toe/Form1.cs
@@ -17,12 +17,30 @@ namespace tic_tac_toe
             InitializeComponent();
         }
 
+        public static bool checkForWinner(int[,] arr, int xo)
+        {
+            bool result = false;
+            for(int i = 0; i < 3; i++)
+            {
+                if (arr[0,i] == xo && arr[1,i] == xo && arr[2,i] == xo)
+                {
+                    result = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (arr[i,0] == xo && arr[i,1] == xo && arr[i,2] == xo)
+                {
+                    result = true;
+                }
+            }
+            if (arr[0,0] == xo && arr[1,1] == xo && arr[2,2] == xo) result = true;
+            if (arr[2,0] == xo && arr[1,1] == xo && arr[0,2] == xo) result = true;
+            return result;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            //TextBox txt = new TextBox();
-            //txt.Text = "hello";
-            //Form1.Controls.Add(txt);
-
             Random rand = new Random();
             int[,] box = new int[3,3];
             for(int r = 0; r < 3; r++)
@@ -30,10 +48,38 @@ namespace tic_tac_toe
                 for(int c = 0; c < 3; c++)
                 {
                     box[r, c] = rand.Next(0, 2);
-                    winner_box.Text += box[r, c];
                 }
             }
-            
+            bool xWins = checkForWinner(box, 1);
+            bool oWins = checkForWinner(box, 0);
+            string winnerText;
+            if(xWins && oWins)
+            {
+                winnerText = "Tie - both are winners!";
+            }
+            else if (xWins)
+            {
+                winnerText = "X wins!";
+            }
+            else if (oWins)
+            {
+                winnerText = "O wins!";
+            }
+            else
+            {
+                winnerText = "Tie - nobody wins.";
+            }
+            winner_box.Text = winnerText;
+        }
+
+        private void new_game_btn_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
